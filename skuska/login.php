14433a6fa5f4a2f40b4fb9aecb36132c2c727ba0
Ondrejjjj <ondrejsmolarik@gmail.com>
2024-04-13 14:18:13 +0200

    este raz

--- skuska/login.php
+++ skuska/login.php
@@ -39,11 +39,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-<<<<<<< HEAD
         $sql = "SELECT id, `username`, password FROM login WHERE `username` = ?";
-=======
-        $sql = "SELECT id, `pouzivatelove meno`, heslo FROM login WHERE `pouzivatelove meno` = ?";
->>>>>>> main
         
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -72,11 +68,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-<<<<<<< HEAD
                             header("location: index.php");
-=======
-                            header("location: welcome.php");
->>>>>>> main
                         } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
